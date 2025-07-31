1cd0107a96ff2ff750ad281907c1ff6221f58017
Victoire07 <victoireboutin07@gmail.com>
2025-07-31 15:57:45 +0200

    Ajout vérification token et affichage conditionnel du lien login

--- part4/script.js
+++ part4/script.js
@@ -36,3 +36,12 @@ document.addEventListener('DOMContentLoaded', () => {
         setupPriceFilter();    // initialise le filtre
     }
 });
+function getCookie(name) {
+    const cookies = document.cookie.split(';');
+    for (let cookie of cookies) {
+        const [key, value] = cookie.trim().split('=');
+        if (key === name) return value;
+    }
+    return null;
+}
+
